f0a8d659a5e6d1fa84321cd8ee42d7416d2788cc
Yacine BOUKARI <steeveboukari9@gmail.com>
2023-01-05 01:14:02 +0100

    build: setup project tree

---
+++ client/src/router/index.ts
@@ -0,0 +1,21 @@
+import {createRouter, createWebHistory, RouteRecordRaw} from "vue-router";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "Home",
+    component: () => import("@/views/HomeView.vue"),
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: () => import("@/views/AboutView.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
