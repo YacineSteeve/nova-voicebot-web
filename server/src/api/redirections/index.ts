b38d602d1746dd569f80e225a44798d232aedd24
Yacine BOUKARI <steeveboukari9@gmail.com>
2023-03-22 06:55:42 +0100

    chore: remove the `server` directory

--- server/src/api/redirections/index.ts
+++
@@ -1,4 +0,0 @@
-import { redirectCompletion, redirectModeration } from './completion';
-import { redirectSpeech } from './speech';
-
-export { redirectCompletion, redirectSpeech, redirectModeration };
--- server/src/controllers/mailing/transporter/index.ts
+++
@@ -1,6 +0,0 @@
-import {createTransport} from 'nodemailer';
-import transport from './transport';
-
-const transporter = createTransport(transport);
-
-export { transporter, transport };
