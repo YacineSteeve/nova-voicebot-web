e109c00fcce9dd9dafc87e55480bd255963893c1
Yacine BOUKARI <steeveboukari9@gmail.com>
2023-03-23 13:16:33 +0100

    fix: `vite.config` imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
-import path = require('path');
+import * as path from 'path';
 
 export default defineConfig({
     plugins: [vue()],
